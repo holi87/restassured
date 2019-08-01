4273cd42e9e30de30410551b7ca5b88b16490886
Holak Grzegorz (FIRMA ZEWNĘTRZNA ) <gholak@pzu.pl>
2019-08-01 14:16:06 +0200

    drobniutki refactor

--- src/test/java/SimpleGetTest.java
+++ src/test/java/SimpleGetTest.java
@@ -1,4 +1,6 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -7,17 +9,41 @@ import org.testng.annotations.Test;
 
 public class SimpleGetTest {
     private String uri = "http://restapi.demoqa.com/utilities/weather/city";
-
-    @Test
-    public void positiveGetTest() {
-        RequestSpecification httpRequest = setHttpRequest(uri);
-        sendGet(httpRequest, "/Hyderabad");
-    }
+    private RequestSpecification httpRequest = setHttpRequest(uri);
 
     @Test
     public void negativeGetTest() {
-        RequestSpecification httpRequest = setHttpRequest(uri);
-        sendGet(httpRequest, "/xdafafsa");
+        Response response = sendGet(httpRequest, "/test");
+        printHeader(response);
+        printBody(response);
+        Assert.assertEquals(response.statusCode(), 200, "Incorrect status code");
+
+    }
+    @Test
+    public void positiveGetTest() {
+        Response response = sendGet(httpRequest, "/Hyderabad");
+        printHeader(response);
+        printBody(response);
+        Assert.assertEquals(response.statusCode(), 200, "Incorrect status code");
+
+    }
+
+    private void printHeader(Response response) {
+        Headers allHeaders = response.headers();
+        System.out.println("Header: ");
+        for (Header header :
+                allHeaders) {
+            System.out.println(header.getName() + " : " + header.getValue());
+
+        }
+    }
+
+    private Response sendGet(RequestSpecification httpRequest, String endpoint) {
+        return httpRequest.request(Method.GET, endpoint);
+    }
+
+    private void printBody(Response response) {
+        System.out.println("Response body: \n" + response.getBody().asString());
     }
 
     private RequestSpecification setHttpRequest(String uri) {
@@ -25,11 +51,5 @@ public class SimpleGetTest {
         return RestAssured.given();
     }
 
-    private void sendGet(RequestSpecification httpRequest, String s) {
-        Response response = httpRequest.request(Method.GET, s);
-        int statusCode = response.statusCode();
-        System.out.println("Response body: " + response.getBody().asString());
-        Assert.assertEquals(statusCode, 200, "Incorrect status code");
-    }
 
 }
