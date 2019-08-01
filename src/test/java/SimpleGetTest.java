2bf55e2dff903f906086be33c312b1c865f5080f
Holak Grzegorz (FIRMA ZEWNĘTRZNA ) <gholak@pzu.pl>
2019-08-01 12:28:34 +0200

    modification first positive and negative get tests

--- src/test/java/SimpleGetTest.java
+++ src/test/java/SimpleGetTest.java
@@ -1,4 +1,3 @@
-
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
@@ -7,22 +6,29 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SimpleGetTest {
+    private String uri = "http://restapi.demoqa.com/utilities/weather/city";
 
     @Test
-    public void GetWeatherDetails() {
-        RequestSpecification httpRequest = setHttpRequest();
+    public void positiveGetTest() {
+        RequestSpecification httpRequest = setHttpRequest(uri);
         sendGet(httpRequest, "/Hyderabad");
+    }
+
+    @Test
+    public void negativeGetTest() {
+        RequestSpecification httpRequest = setHttpRequest(uri);
         sendGet(httpRequest, "/xdafafsa");
     }
 
-    private RequestSpecification setHttpRequest() {
-        RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+    private RequestSpecification setHttpRequest(String uri) {
+        RestAssured.baseURI = uri;
         return RestAssured.given();
     }
 
     private void sendGet(RequestSpecification httpRequest, String s) {
         Response response = httpRequest.request(Method.GET, s);
         int statusCode = response.statusCode();
+        System.out.println("Response body: " + response.getBody().asString());
         Assert.assertEquals(statusCode, 200, "Incorrect status code");
     }
 
